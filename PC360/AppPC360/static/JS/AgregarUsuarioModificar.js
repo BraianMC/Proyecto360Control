9cbec2f8c9d5b981d52c4bf73e214ff8e00a5235
BraianMC <braianmc9@gmail.com>
2023-12-14 17:45:24 -0300

    Modificaciones de estilo y script

--- PC360/AppPC360/static/JS/AgregarUsuarioModificar.js
+++ PC360/AppPC360/static/JS/AgregarUsuarioModificar.js
@@ -8,14 +8,12 @@ function agregarUsuarioModificar() {
     var usuarioSeleccionadom = selectUsuariosm.options[selectUsuariosm.selectedIndex].text;
     var rolSeleccionadom = selectRolesm.options[selectRolesm.selectedIndex].text;
 
-    // Validar que se haya seleccionado un usuario
+    // Valida que se haya seleccionado un usuario
     if (usuarioSeleccionadom) 
     {
-
         var li = document.createElement('li');
-        // Configurar el contenido del li
         li.innerHTML = '<span class="content-span">' + usuarioSeleccionadom + '</span>/<span class="content-span">' + rolSeleccionadom + '</span>' +
-            '<div class="icon-container"><i class="fa-solid fa-pencil btn-pencil-2 btn-update-1"></i>' + '<i class="fa-solid fa-rectangle-xmark btn-xmark"></i></div>';
+            '<div class="icon-container"><i class="fa-solid fa-rectangle-xmark btn-xmark"></i></div>';
         // Agregar el li a la lista de usuarios asignados
         userListm.appendChild(li);
         alert("Usuario agregado al listado!.");
